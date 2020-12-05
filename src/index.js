d27386ec43890be14b6eca9d00acb794d646bcfb
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2020-12-05 17:04:06 +0530

    database is working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 function Add(){
+    CreateDb();
     const div=document.getElementById("result");
     const lresult=document.createElement("label");
     lresult.setAttribute("class","lresult");
     div.append(lresult);
     lresult.innerHTML="Added Successfully";
+}
+function CreateDb(){
+    let request=window.indexedDB.open("studentData",1);
+    var db;
+    request.onupgradeneeded=e=>{
+        db=e.target.result;
+        console.log(`in onupgradedneeded ${db}`);
+        db.createObjectStore("student_records",{keyPath:"RollNo"});
+    }
+    request.onsuccess=function(event){
+        db=event.target.result;
+        let temp=request.result;
+        console.log("DataBase Created!!");
+        const tx=db.transaction("student_records","readwrite");
+        const sRecords=tx.objectStore("student_records");
+        const rollno=document.getElementById("rollNo").value;
+        const name=document.getElementById("name").value;
+        const marks=document.getElementById("marks").value;
+        const data={
+            RollNo:rollno,
+            Name:name,
+            Marks:marks
+        };
+        sRecords.add(data);
+    }
 }
