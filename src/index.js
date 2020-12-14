b03e9690ec2380cf89642c031483faf4eb7837b2
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2020-12-14 20:03:44 +0530

    Search & Add is working good

--- src/index.js
+++ src/index.js
@@ -1,33 +1,85 @@
-function Add(){
-    CreateDb();
-    const div=document.getElementById("result");
-    const lresult=document.createElement("label");
-    lresult.setAttribute("class","lresult");
-    div.append(lresult);
-    lresult.innerHTML="Added Successfully";
-}
-function CreateDb(){
-    let request=window.indexedDB.open("studentData",1);
-    var db;
-    request.onupgradeneeded=e=>{
-        db=e.target.result;
-        console.log(`in onupgradedneeded ${db}`);
-        db.createObjectStore("student_records",{keyPath:"RollNo"});
+function Add() {
+    let request = window.indexedDB.open("studentData", 1);
+    request.onupgradeneeded = e => {
+        db = e.target.result;
+        db.createObjectStore("student_records", { keyPath: "RollNo" });
     }
-    request.onsuccess=function(event){
-        db=event.target.result;
-        let temp=request.result;
+    request.onsuccess = function (event) {
+        db = event.target.result;
+        let temp = request.result;
         console.log("DataBase Created!!");
-        const tx=db.transaction("student_records","readwrite");
-        const sRecords=tx.objectStore("student_records");
-        const rollno=document.getElementById("rollNo").value;
-        const name=document.getElementById("name").value;
-        const marks=document.getElementById("marks").value;
-        const data={
-            RollNo:rollno,
-            Name:name,
-            Marks:marks
+        const tx = db.transaction("student_records", "readwrite");
+        const sRecords = tx.objectStore("student_records");
+        const rollno = document.getElementById("rollNo").value;
+        const name = document.getElementById("name").value;
+        const marks = document.getElementById("marks").value;
+        const data = {
+            RollNo: rollno,
+            Name: name,
+            Marks: marks
         };
         sRecords.add(data);
+        const div = document.getElementById("result");
+        const lresult = "<label>Added Successfully</label>"
+        div.innerHTML = lresult;
     }
+    request.onerror = () => {
+        const div = document.getElementById("result");
+        const lresult = "<label>Can't Add data</label>"
+        div.innerHTML = lresult;
+    }
+}
+function Search() {
+    let request = window.indexedDB.open("studentData", 1);
+    request.onupgradeneeded = e => {
+        e.target.transaction.abort();
+        const div = document.getElementById("result");
+        const lresult = "<label>Database doesn't exists</label>"
+        div.innerHTML = lresult;
+    }
+    request.onsuccess = function (event) {
+        db = event.target.result;
+        const tx = db.transaction("student_records", "readwrite");
+        const sRecords = tx.objectStore("student_records");
+        const rollno = document.getElementById("rollNo").value;
+        const data = sRecords.get(rollno);
+        data.onsuccess = () => {
+            if (typeof data.result != "undefined") {
+                const syntax=`<table>
+                <tr>
+                    <th>Name:</th>
+                    <td>${data.result.Name}</td>
+                </tr>
+                <tr>
+                    <th>Roll No:</th>
+                    <td>${data.result.RollNo}</td>
+                </tr>
+                <tr>
+                    <th>Marks:</th>
+                    <td>${data.result.Marks}</td>
+                </tr>
+                </table>`
+                const div = document.getElementById("result");
+                div.innerHTML = syntax;
+            }
+            else{
+                const div = document.getElementById("result");
+                const lresult = "<label>No Record found</label>"
+                div.innerHTML = lresult;
+            }
+        }
+        data.onerror = () => {
+            const div = document.getElementById("result");
+            const lresult = "<label>Error:Can't open record</label>"
+            div.innerHTML = lresult;
+        }
+    }
+    request.onerror = () => {
+        const div = document.getElementById("result");
+        const lresult = "<label>Error:Can't open database</label>"
+        div.innerHTML = lresult;
+    }
+}
+function Delete() {
+
 }
